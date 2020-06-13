b93f08bf46bf3959274bee10a475c9745369e28d
Diego Barranco <dbarrancolinan@gmail.com>
2020-06-13 18:13:21 -0400

    Added coc-settings.json and setup.sh

---
+++ setup.sh
@@ -0,0 +1,4 @@
+ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+ln -s ~/Git/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Git/dotfiles/.config/nvim/init.vim
+ln -s ~/Git/dotfiles/.config/nvim/coc-settings.json
