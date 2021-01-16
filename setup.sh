2c8356c09c3b745eb94c17949f4ad3999df0e679
Barlingo <dbarrancolinan@gmail.com>
2021-01-16 16:24:08 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+touch ~/.config/gtk-3.0/gtk.css   
 ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+touch ~/.vimrc
 ln -s ~/Git/dotfiles/.vimrc ~/.vimrc
+touch ~/.config/nvim/init.vim
 ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+touch ~/.config/nvim/coc-settings.json
 ln -s ~/Git/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
