e1db1e8ba8e41646d632283a3c3a751ecc7d99a8
Diego Barranco <dbarranco@xps15.lan>
2021-01-21 21:29:59 -0500

    test

--- README.md
+++ README.md
@@ -6,18 +6,3 @@ ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
 ln -s ~/Git/dotfiles/.vimrc ~/.vimrc
 ```
 
-__Install fingerprint reader__
-```
-sources: (you are only a genius if you don't reveal your sources) https://askubuntu.com/questions/1239582/xps-13-9300-fingerprint-reader-any-driver-updates https://askubuntu.com/questions/1015416/use-fingerprint-authentication-not-only-for-login
-
-sudo apt install libfprint-2-tod1
-
-wget http://dell.archive.canonical.com/updates/pool/public/libf/libfprint-2-tod1-goodix/libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
-
-sudo dpkg -i libfprint-2-tod1-goodix_0.0.4-0ubuntu1somerville1_amd64.deb
-
-sudo pam-auth-update
-#Enable "Fingerprint authentication" using spacebar
-
-# Restart and go to users in the system settings
-```
--- setup.sh
+++ setup.sh
@@ -1,12 +1,4 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-touch ~/.config/gtk-3.0/gtk.css   
-ln -s .config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
-touch ~/.vimrc
-ln -s vimrc ~/.vimrc
-touch ~/.config/nvim/init.vim
-ln -s .config/nvim/init.vim ~/.config/nvim/init.vim
-touch ~/.config/nvim/coc-settings.json
-ln -s .config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
-
-cp --parents .local/share/applications/whatsapp-desktop-webapp.desktop ~
-cp --parents .local/share/icons/hicolor/128x128/apps/whatsapp_web.png ~
+ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+ln -s ~/Git/dotfiles/.vimrc ~/.vimrc
+ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+ln -s ~/Git/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
