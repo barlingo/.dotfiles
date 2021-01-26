d3753da5d6f863ad47bad3f1075c45624611a0b4
Diego Barranco <dbarranco@xps15.lan>
2021-01-25 20:36:50 -0500

    added python language server install

--- .config/nvim/install.sh
+++ .config/nvim/install.sh
@@ -8,3 +8,7 @@ for f in `find . -regex ".*\.vim$\|.*\.lua$"`; do
     rm -rf ~/.config/nvim/$f
     ln $f ~/.config/nvim/$f
 done
+
+# Required language server for python
+pip install 'python-language-server[all]'
+# clang will require manual instalation
