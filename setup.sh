8ef96260989b803950dcf2b0c52df85e4aed5486
Barlingo <dbarrancolinan@gmail.com>
2021-07-09 22:27:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ rm ~/.config/gtk-3.0/gtk.css
 ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
 rm ~/.config/nvim/lv-config.lua
 ln -s ~/Git/dotfiles/.config/nvim/lv-config.lua ~/.config/nvim/lv-config.lua
+rm ~/.tmux.conf
+ln -s ~/Git/dotfiles/.tmux.conf ~/.tmux.conf
