bb250545b6b5b3af5e3aa7257c04542ae205fd3b
Barlingo <dbarrancolinan@gmail.com>
2021-07-09 22:08:41 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,2 @@
 ln -s ~/Git/dotfiles/.config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
-ln -s ~/Git/dotfiles/.vimrc ~/.vimrc
-ln -s ~/Git/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/Git/dotfiles/.config/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
+ln -s ~/Git/dotfiles/.config/nvim/lv-config.lua ~/.config/nvim/lv-config.luga
