018541f580fe71d0b32d74f516070c8ba485f75a
Diego Barranco <dbarrancolinan@gmail.com>
2021-07-15 22:05:56 -0400

    Added darkplus theme. Updated to rolling...

--- nvim/.config/nvim/lv-config.lua
+++ nvim/.config/nvim/lv-config.lua
@@ -1,82 +1,50 @@
 --[[
 O is the global options object
+
 Linters should be
 filled in as strings with either
 a global executable or a path to
 an executable
 ]]
 -- THESE ARE EXAMPLE CONFIGS FEEL FREE TO CHANGE TO WHATEVER YOU WANT
+
 -- general
 O.format_on_save = true
 O.auto_complete = true
-O.colorscheme = "spacegray"
 O.auto_close_tree = 0
-O.wrap_lines = false
+O.wrap = false
 O.timeoutlen = 100
-O.leader_key = " "
-O.ignore_case = true
-O.smart_case = true
-O.relative_number = true
-O.colorcolumn = "80"
-O.cmdheight = 2
-O.tab_stop = 4
-O.shift_width = 4
-O.transparent_window = 1
-O.nvim_tree_disable_netrw = 1
+O.default_options.relativenumber = true
+O.default_options.colorcolumn = "80"
+O.default_options.tabstop = 4
+O.colorscheme = "darkplus"
+O.default_options.shiftwidth = 4
 
-
-
--- TODO User Config for predefined plugins
+-- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 O.plugin.dashboard.active = true
-O.plugin.colorizer.active = true
-O.plugin.ts_playground.active = false
-O.plugin.indent_line.active = true
+O.plugin.terminal.active = true
+O.plugin.dap.active = true
 O.plugin.zen.active = false
-O.plugin.floatterm.active = true
-O.plugin.telescope_project.active = true
-O.plugin.telescope_fzy.active = true
--- O.plugin.barbar.active = false
--- O.plugin.lazygit.active = true
-
--- dashboard
--- O.dashboard.custom_header = {"hi"}
--- O.dashboard.footer = {"hey"}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 O.treesitter.ensure_installed = {}
 O.treesitter.ignore_install = { "haskell" }
 O.treesitter.highlight.enabled = true
 
--- pythonRe
--- O.lang.python.linter = 'flake8'
+-- python
+O.lang.python.linter = "flake8"
 O.lang.python.isort = true
 O.lang.python.diagnostics.virtual_text = true
 O.lang.python.analysis.use_library_code_types = true
+-- to change default formatter from yapf to black
+-- O.lang.python.formatter.exe = "black"
+-- O.lang.python.formatter.args = {"-"}
+
+-- go
+-- to change default formatter from gofmt to goimports
+-- O.lang.formatter.go.exe = "goimports"
 
 -- javascript
 O.lang.tsserver.linter = nil
-
--- Additional Plugins
--- O.user_plugins = {
-
---     {"folke/tokyonight.nvim"}, {
---         "ray-x/lsp_signature.nvim",
---         config = function() require"lsp_signature".on_attach() end,
---         event = "InsertEnter"
---     }
--- }
-
--- Autocommands (https://neovim.io/doc/user/autocmd.html)
--- O.user_autocommands = {{ "BufWinEnter", "*", "echo \"hi again\""}}
-
--- Additional Leader bindings for WhichKey
--- O.user_autocommands= {{ "BufWinEnter", "*", "set guifont=FiraCode\\ Nerd\\ Font\\ Mono:h13"}}
-O.user_which_key = {
-  t = {
-    name = "+terminal",
-    t = {"<cmd>lua require('FTerm').toggle()<cr>","toggle"},
-  },
-}
-
 
