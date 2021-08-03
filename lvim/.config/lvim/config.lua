e82b3ae1c3996b43ad6e317ae28d3c4b157d1621
Diego Barranco <dbarrancolinan@gmail.com>
2021-08-02 22:48:27 -0400

    Added transparent window to themes

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -15,6 +15,7 @@ vim.opt.guifont = "Hack Regular Nerd Font Complete:h12"
 require("lv-extra-fn").vim_plugins(false)
 -- Colorscheme
 lvim.colorscheme = "spacegray"
+lvim.transparent_window = false
 
 -- On save autocmd
 lvim.format_on_save = true
