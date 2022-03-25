87a747a2b22b48264f3787a66d642707418f73f2
Diego Barranco <dbarrancolinan@gmail.com>
2022-03-25 11:50:56 -0400

    Fixed minimap setup error.

--- lvim/.config/lvim/lua/config/minimap.lua
+++ lvim/.config/lvim/lua/config/minimap.lua
@@ -2,5 +2,8 @@ local M = {}
 
 M.setup = function()
 	vim.cmd("let g:minimap_width = 20")
-	-- vim.cmd("let g:minimap_auto_start = 0")
+	vim.cmd("let g:minimap_auto_start = 0")
+	vim.cmd("let g:minimap_auto_start_win_enter = 0")
 end
+
+return M
