1941c18b9de78c4535eee07230cacb87b59d8c35
Diego Barranco <dbarrancolinan@gmail.com>
2022-05-16 23:51:21 -0400

    Removed vim.opt from lvim. It seems to cause issues with nvim-cmp.

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -1,14 +1,4 @@
 --  General Settings
-vim.opt.relativenumber = true
-vim.opt.wrap = false
-vim.opt.timeoutlen = 200
-vim.opt.colorcolumn = "80"
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.cmdheight = 1
-vim.opt.foldmethod = "expr"
-vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-vim.opt.foldlevel = 5
 
 -- Remove Vim plugins not used
 -- require("lv-extra-fn").vim_plugins(false)
@@ -24,7 +14,6 @@ lvim.format_on_save = true
 lvim.lint_on_save = true
 
 -- LSP Diagnostics
-lvim.lsp.diagnostics.virtual_text = false
 lvim.lsp.diagnostics.show_line_diagnostics = false
 
 -- Optional plugins LunarVim
@@ -36,15 +25,15 @@ lvim.builtin.notify.active = true
 -- Builtin plugns settings
 lvim.builtin.nvimtree.show_icons.git = 1
 lvim.builtin.treesitter.ensure_installed = "all"
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+-- lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
-lvim.builtin.bufferline.options.diagnostics = false
+lvim.builtin.bufferline.options.diagnostics = true
 
 if vim.fn.has("wsl") == 1 then
 	require("lv-wsl")
 end
 
 require("lv-plugins")
-require("lv-autocommands")
+-- require("lv-autocommands")
 require("lv-keymappings")
--- lvim/.config/lvim/lua/lv-autocommands.lua
+++ lvim/.config/lvim/lua/lv-autocommands.lua
@@ -1,11 +1,11 @@
--- -- Autocommands
--- lvim.autocommands.custom_groups = {
--- 	-- python
--- 	{
--- 		"Filetype",
--- 		"python",
--- 		"nnoremap <leader>xr <cmd>lua require('toggleterm.terminal')Terminal:new (on "
--- 			.. vim.fn.expand("%")
--- 			.. ";read')<CR>",
--- 	},
--- }
+-- Autocommands
+lvim.autocommands.custom_groups = {
+	-- python
+	{
+		"Filetype",
+		"python",
+		"nnoremap <leader>xr <cmd>lua require('toggleterm.terminal')Terminal:new (on "
+			.. vim.fn.expand("%")
+			.. ";read')<CR>",
+	},
+}
