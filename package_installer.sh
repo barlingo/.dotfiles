a73148d4b243887cd8be83c3c54bb7c95d537262
barlingo <--global>
2022-05-26 00:09:12 -0400

    [conf] working pyenv under .local/share

--- package_installer.sh
+++ package_installer.sh
@@ -1,6 +1,6 @@
 export RUSTUP_HOME=$HOME/.local/share/rustup
 export RUSTUP_HOME=$HOME/.local/share/rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh 
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source $HOME/.local/share/cargo/env
 cargo install fnm
 
@@ -8,10 +8,9 @@ cargo install fnm
 export NVM_DIR="$HOME/.local/share/nvm"
 mdkir $HOME/local/share/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install --lts
 
 #install pyenv with virtualenv
 git clone https://github.com/pyenv/pyenv.git ~/.local/share/pyenv
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
-
--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -1,7 +1,2 @@
 #!/bin/sh
-export RUSTUP_HOME=$HOME/.local/share/rustup
-export RUSTUP_HOME=$HOME/.local/share/rustup
-export NVM_DIR="$HOME/.local/share/nvm"
-source $HOME/.local/share/cargo/env
-
 source $ZDOTDIR/zshrc
