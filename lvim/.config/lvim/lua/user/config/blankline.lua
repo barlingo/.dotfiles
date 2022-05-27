6f900edb0e07fff2fac123b3326ba49f9dff761d
barlingo <--global>
2022-05-27 15:52:57 -0400

    [feat] added NL and space character to indentLine. Added TS context.

--- lvim/.config/lvim/lua/user/config/blankline.lua
+++ lvim/.config/lvim/lua/user/config/blankline.lua
@@ -14,6 +14,15 @@ M.setup = function()
     "NvimTree",
     "Trouble",
   }
+  vim.opt.list = true
+  vim.opt.listchars:append("space:⋅")
+  vim.opt.listchars:append("eol:↴")
+
+  require("indent_blankline").setup({
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+  })
   -- vim.g.indent_blankline_char = "│"
   vim.g.indent_blankline_char = "▏"
   vim.g.indent_blankline_show_trailing_blankline_indent = false
