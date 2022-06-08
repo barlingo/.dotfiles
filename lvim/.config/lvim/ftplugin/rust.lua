075b68cbbc7e3c54fd87f0e44e82f15c0d254738
barlingo <16936512+barlingo@users.noreply.github.com>
2022-06-08 16:50:44 -0400

    [ref] refactor some stuff like rust dap not working atm. Removed eol character.

--- lvim/.config/lvim/ftplugin/rust.lua
+++ lvim/.config/lvim/ftplugin/rust.lua
@@ -1,2 +1,7 @@
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({ { exe = "rustfmt", filetypes = { "rust" } } })
+
+if lvim.builtin.dap.active then
+  local dap_install = require("dap-install")
+  dap_install.config("codelldb", {})
+end
